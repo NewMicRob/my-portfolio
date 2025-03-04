9aed82b009e377e8a3b7ee5ef6de8d4c1158c677
NewMicRob <microbnew@gmail.com>
2025-03-04 16:04:08 -0500

    alerts to other buttons

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ const project1btn = document.getElementById("project1");
     project1btn.addEventListener("click", function() {
         alert("More Details");
 })
+
+const project2btn = document.getElementById("project2");
+    project12btn.addEventListener("click", function() {
+        alert("More Details");
+})
+
+const project3btn = document.getElementById("project3");
+    project3btn.addEventListener("click", function() {
+        alert("More Details");
+})
+
+const project4btn = document.getElementById("project4");
+    project4btn.addEventListener("click", function() {
+        alert("More Details");
+})

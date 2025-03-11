8426344d3e833f198e1395f6013bcb555c2d54bc
NewMicRob <microbnew@gmail.com>
2025-03-10 21:51:26 -0400

    fixed (gallery) button interactivity and styled form

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 const project1btn = document.getElementById("project1");
-    project1btn.addEventListener("click", function() {
-        alert("More Details");
-})
+const project1div = document.getElementById("p1info");
+project1btn.addEventListener("click", function() {
+    p1info.style.display = (p1info.style.display === "none") ? "block" : "none";
+});
 
 const project2btn = document.getElementById("project2");
-    project2btn.addEventListener("click", function() {
-        alert("More Details");
-})
+const project2div = document.getElementById("p2info");
+project2btn.addEventListener("click", function() {
+    p2info.style.display = (p2info.style.display === "none") ? "block" : "none";
+});
 
 const project3btn = document.getElementById("project3");
-    project3btn.addEventListener("click", function() {
-        alert("More Details");
-})
+const project3div = document.getElementById("p3info");
+project3btn.addEventListener("click", function() {
+    p3info.style.display = (p3info.style.display === "none") ? "block" : "none";
+});
 
 const project4btn = document.getElementById("project4");
-    project4btn.addEventListener("click", function() {
-        alert("More Details");
-})
+const project4div = document.getElementById("p4info");
+project4btn.addEventListener("click", function() {
+    p4info.style.display = (p4info.style.display === "none") ? "block" : "none";
+});
+

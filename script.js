73c868a7938e4a1d7b403f0e89663d807ad6e1d6
NewMicRob <microbnew@gmail.com>
2025-04-19 23:21:30 -0400

    reworked nav, style tweaks, added funtionality to buttons, and added from validation.

--- script.js
+++ script.js
@@ -1,41 +1,66 @@
 const project1btn = document.getElementById("project1");
-const project1div = document.getElementById("p1info");
 project1btn.addEventListener("click", function() {
-    p1info.style.display = (p1info.style.display === "none") ? "block" : "none";
+    window.open("https://newmicrob.github.io/Lab-7-JavaScript-Events/", "_blank");
 });
 
 const project2btn = document.getElementById("project2");
-const project2div = document.getElementById("p2info");
 project2btn.addEventListener("click", function() {
-    p2info.style.display = (p2info.style.display === "none") ? "block" : "none";
+    window.open("https://newmicrob.github.io/accessible-registration/", "_blank");
 });
 
 const project3btn = document.getElementById("project3");
-const project3div = document.getElementById("p3info");
 project3btn.addEventListener("click", function() {
-    p3info.style.display = (p3info.style.display === "none") ? "block" : "none";
+    window.open("https://newmicrob.github.io/Assignment-2-Build-a-Web-Page-Using-the-HTML5-Web-Storage-API/", "_blank");
 });
 
 const project4btn = document.getElementById("project4");
-const project4div = document.getElementById("p4info");
 project4btn.addEventListener("click", function() {
-    p4info.style.display = (p4info.style.display === "none") ? "block" : "none";
+    window.open("https://newmicrob.github.io/Group-Recipe-Project/", "_blank");
 });
 
 function validateForm() {
-    let form = document.forms["form"];
+    alert("Validating form...");
     let fName = form["fName"].value;
     let lName = form["lName"].value;
     let email = form["email"].value;
     let tel = form["tel"].value;
+    let honorifics = document.querySelectorAll('input[name="honorific"]:checked');
+    if (honorifics.length === 0) {
+        alert("Please select an honorific.");
+        return false;
+    }
+
     if (fName === "" || lName === "") {
         alert("Please fill in the * fields");
         return false;
-    } else if(email === "") {
+    }
+
+    if (email == "" || !validateEmail(email)) {
         alert("Please use standard email format i.e. example@email.com");
-    return false;
-    } else if(tel === "") {
-        alert("Please enter numbers i.e. 123-123-1234");
+        return false;
+    }
+
+    if (tel == "" || !validatePhone(tel)) { 
+        alert("Please enter phone number in the format i.e. 123-123-1234");
+        return false;
     }
     return true;
+}
+
+function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function validatePhone(tel) {
+    const re = /^\d{3}-\d{3}-\d{4}$/;
+    return re.test(String(tel));
+}
+
+function openNav() {
+    document.getElementById('navBar').style.width = "250px"
+}
+
+function closeNav() {
+    document.getElementById('navBar').style.width = "0";
 }
